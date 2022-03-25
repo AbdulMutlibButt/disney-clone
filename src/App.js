26e0d4bee2e8a995ca592bff8ea2aed144021487
AbdulMutlibB <mutlibbutt209@gmail.com>
2022-03-26 01:12:36 +0500

    signOut functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import Header from "./components/Header";
+import Home from "./components/Home";
 function App() {
   return (
     <Router>
       <div>
+        <Header />
         <Switch>
-          <Route  path="/">
-            <Header/>
+          <Route exact path="/">
             <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
           </Route>
         </Switch>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,90 +2,133 @@ import React from "react";
 import { auth, provider } from "../firebase";
 import "./Header.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from 'react-router-dom'
-import { selectUserName, selectUserPhoto, setUserLoginDetails } from '../features/user/userSlice'
+import { useHistory } from "react-router-dom";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setUserLoginDetails,
+} from "../features/user/userSlice";
+import { useEffect } from "react";
 function Header() {
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const username = useSelector(selectUserName)
-  const userPhoto = useSelector(selectUserPhoto)
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
 
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        history.push("/home");
+      }
+    });
+  }, [userName]);
 
   const handleAuth = () => {
     auth
       .signInWithPopup(provider)
       .then((result) => {
-        setUser(result.user)
+        setUser(result.user);
       })
       .catch((e) => {
         alert(e.message);
       });
+  };
+  const logOut = () => {
+    auth.signOut().then(() => {
+      history.push("/");
+      setUser("");
+    });
   };
   const setUser = (user) => {
     dispatch(
       setUserLoginDetails({
         name: user.displayName,
         email: user.email,
-        photo: user.photoURL
+        photo: user.photoURL,
       })
-    )
-  }
+    );
+  };
+  console.log(userPhoto);
   return (
     <div className="header_navbar">
       <img className="header_logo" src="/images/logo.svg" alt="" />
-      {!username ? <p onClick={handleAuth} className="header_navmenu_login">
-        LOGIN
-      </p> : <>
-        <div className="header_navmenu">
-          <a className="header_navmenu_link" href="/home">
-            <img
-              className="header_navmenu_img"
-              src="/images/home-icon.svg"
-              alt=""
-            />
-            <span className="header_navmenu_span">HOME</span>
-          </a>
-          <a className="header_navmenu_link" href="/home">
-            <img
-              className="header_navmenu_img"
-              src="/images/search-icon.svg"
-              alt=""
-            />
-            <span className="header_navmenu_span">SEARCH</span>
-          </a>
-          <a className="header_navmenu_link" href="/home">
-            <img
-              className="header_navmenu_img"
-              src="/images/watchlist-icon.svg"
-              alt=""
-            />
-            <span className="header_navmenu_span">WATCHLIST</span>
-          </a>
-          <a className="header_navmenu_link" href="/home">
-            <img
-              className="header_navmenu_img"
-              src="/images/movie-icon.svg"
-              alt=""
-            />
-            <span className="header_navmenu_span">MOVIES</span>
-          </a>
-          <a className="header_navmenu_link" href="/home">
-            <img
-              className="header_navmenu_img"
-              src="/images/series-icon.svg"
-              alt=""
-            />
-            <span className="header_navmenu_span">SERIES</span>
-          </a>
-        </div>
+      {!userName ? (
         <p onClick={handleAuth} className="header_navmenu_login">
           LOGIN
         </p>
-        <img className="header_navmenu_userphoto" src={userPhoto} alt={username} />
-      </>}
+      ) : (
+        <>
+          <div className="header_navmenu">
+            <a className="header_navmenu_link" href="/home">
+              <img
+                className="header_navmenu_img"
+                src="/images/home-icon.svg"
+                alt=""
+              />
+              <span className="header_navmenu_span">HOME</span>
+            </a>
+            <a className="header_navmenu_link" href="/home">
+              <img
+                className="header_navmenu_img"
+                src="/images/search-icon.svg"
+                alt=""
+              />
+              <span className="header_navmenu_span">SEARCH</span>
+            </a>
+            <a className="header_navmenu_link" href="/home">
+              <img
+                className="header_navmenu_img"
+                src="/images/watchlist-icon.svg"
+                alt=""
+              />
+              <span className="header_navmenu_span">WATCHLIST</span>
+            </a>
+            <a className="header_navmenu_link" href="/home">
+              <img
+                className="header_navmenu_img"
+                src="/images/movie-icon.svg"
+                alt=""
+              />
+              <span className="header_navmenu_span">MOVIES</span>
+            </a>
+            <a className="header_navmenu_link" href="/home">
+              <img
+                className="header_navmenu_img"
+                src="/images/series-icon.svg"
+                alt=""
+              />
+              <span className="header_navmenu_span">SERIES</span>
+            </a>
+          </div>
+          <div className="header_navmenu_signOut">
+            <div>
+              <img
+                className="header_navmenu_userphoto"
+                src={`${userPhoto}`}
+                alt={userName}
+              />
+            </div>
+            <div className="header_navmenu_dropdown">
+              <div
+                onClick={() => {
+                  logOut();
+                }}
+                className="header_navmenu_logOut"
+              >
+                logOut
+              </div>
+            </div>
+          </div>
 
+          {/* <img
+            className="header_navmenu_userphoto"
+            src={`${userPhoto}`}
+            alt={userName}
+          /> */}
+        </>
+      )}
     </div>
-
   );
 }
 export default Header;
